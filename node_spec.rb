0e7041c4bb631a30e885e613719b1f1feec0f8c5
Leo Sun <leo.tcsun@gmail.com>
2013-07-18 19:18:25 -0400

    added node class

--- node_spec.rb
+++ node_spec.rb
@@ -4,21 +4,21 @@ require "spec_helper.rb"
 describe Node do
 
   it "knows its own name" do
-    node = Node.new("E")
-    node.name.should == "E"
+    node = Node.new("5")
+    node.name.should == "5"
   end
 
   it "reads graph from edge files" do
-    node = Node.new("E")
+    node = Node.new("5")
     node.read_edge_file("edges.sample")
     node.graph.should_not be_nil
   end
 
   it "finds out its neibours form the graph" do
-    node = Node.new("E")
+    node = Node.new("5")
     node.read_edge_file("edges.sample")
 
-    neibours = node.graph["E"].keys
+    neibours = node.graph["5"].keys
     neibours.should include "C"
     neibours.should include "B"
     neibours.should include "F"
--- reader_spec.rb
+++ reader_spec.rb
@@ -5,40 +5,40 @@ describe "Reader" do
   it "reads edge files" do
     graph = Reader.read_edge_file("edges.sample")
 
-    graph["A"]["C"].should == 1
-    graph["A"]["B"].should == 4
-    graph["C"]["E"].should == 2
-    graph["B"]["E"].should == 3
-    graph["B"]["D"].should == 5
-    graph["D"]["G"].should == 2
-    graph["E"]["F"].should == 1
-    graph["F"]["G"].should == 4
-    graph["F"]["D"].should == 2
-    graph["E"]["G"].should == 6
+    graph["1"]["3"].should == 1
+    graph["1"]["2"].should == 4
+    graph["3"]["5"].should == 2
+    graph["2"]["5"].should == 3
+    graph["2"]["4"].should == 5
+    graph["4"]["7"].should == 2
+    graph["5"]["6"].should == 1
+    graph["6"]["7"].should == 4
+    graph["6"]["4"].should == 2
+    graph["5"]["7"].should == 6
 
-    graph["C"]["A"].should == 1
-    graph["B"]["A"].should == 4
-    graph["E"]["C"].should == 2
-    graph["E"]["B"].should == 3
-    graph["D"]["B"].should == 5
-    graph["G"]["D"].should == 2
-    graph["F"]["E"].should == 1
-    graph["G"]["F"].should == 4
-    graph["D"]["F"].should == 2
-    graph["G"]["E"].should == 6
+    graph["3"]["1"].should == 1
+    graph["2"]["1"].should == 4
+    graph["5"]["3"].should == 2
+    graph["5"]["2"].should == 3
+    graph["4"]["2"].should == 5
+    graph["7"]["4"].should == 2
+    graph["6"]["5"].should == 1
+    graph["7"]["6"].should == 4
+    graph["4"]["6"].should == 2
+    graph["7"]["5"].should == 6
   end
 
   it "reads table files" do
     routes = Reader.read_table_file("table.sample")
 
-    dest_E = routes["E"]
-    dest_E.dest.should == "E"
+    dest_E = routes["5"]
+    dest_E.dest.should == "5"
     dest_E.path_cost.should == 6
     dest_E.path == []
 
-    dest_B = routes["B"]
-    dest_B.dest.should == "B"
+    dest_B = routes["2"]
+    dest_B.dest.should == "2"
     dest_B.path_cost.should == 8
-    dest_B.path == ["F", "E"]
+    dest_B.path == ["6", "5"]
   end
 end
